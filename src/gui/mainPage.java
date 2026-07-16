3153e7440bc0757920dbad7f67c11f423d3d790c
areebasajjal <areebasajjal22@gmail.com>
2026-07-16 19:59:51 +0500

    feat: main java file ( running part )

---
+++ src/gui/mainPage.java
@@ -0,0 +1,11 @@
+package gui;
+
+//run this   starting point of project
+class mainPage {
+  public static void main(String[] args) {
+    Login login = new Login();
+    login.createLoginScreen();
+
+  }
+
+}
